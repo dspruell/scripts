2a17e7e94ba89fbefa1509b3e98e1084406d550b
dspruell <dspruell@sancho2.net>
2024-12-10 23:57:09 -0700

    Add support for stripping deprecated options from IPVanish configs
    - Add -s option to IPVanish import script to automatically strip
      deprecated options from configs that cause connections to fail in more
      recent versions of OpenVPN. An example option is `keysize`.
    - Normalize variable casing and fix some shell syntax

--- utils/ipvanish-import.sh
+++ utils/ipvanish-import.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2023 Darren Spruell <phatbuckett@gmail.com>
+# Copyright (c) 2023-2024 Darren Spruell <phatbuckett@gmail.com>
 # 
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -14,22 +14,26 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  
-# Import IPVanish VPN OpenVPN configurations into NetworkManager.
+# Import IPVanish VPN OpenVPN configurations into NetworkManager. Optionally
+# make useful transformations to the configuration, such as adding a username
+# or removing deprecated options.
 
 set -e
 
-progname="$(basename "$0")"
-new_cnt=0
-del_cnt=0
+PROGNAME="$(basename "$0")"
+DEPRECATED_OPTS_RE='^keysize'
+NEW_CNT=0
+DEL_CNT=0
 
 usage()
 {
     cat <<-EOF
-	USAGE: $progname [options] [config]...
+	USAGE: $PROGNAME [options] [config]...
 	  -u: add IPVanish account username to configs (usually required)
 	  -r: reset/refesh profiles by deleting all IPVanish VPN configs and
 	      importing new ones
 	  -d: delete all IPVanish VPN configs and don't import new ones
+	  -s: strip deprecated IPVanish options from configurations
 	  -h: display this help output
 
 	This script will import IPVanish .ovpn config files into NetworkManager.
@@ -51,27 +55,27 @@ EOF
 # pattern) are deleted.
 reset_vpn()
 {
-    local connlist
     connlist="$(nmcli -t -f NAME,UUID,TYPE connection show \
         | awk -F ':' '($3 == "vpn") && ($1 ~ /^ipvanish-[A-Z][A-Z]-/) {print $2}')"
     for conn_id in $connlist; do
         nmcli connection delete "$conn_id"
-        del_cnt=$((del_cnt+1))
+        DEL_CNT=$((DEL_CNT+1))
     done
 }
 
 # Output activity summary.
 print_status()
 {
-    printf "[DONE] removed %d, added %d configuration(s)\n" $del_cnt $new_cnt
+    printf "[DONE] removed %s, added %s configuration(s)\n" "$DEL_CNT" "$NEW_CNT"
 }
 
 # Parse script arguments.
-while getopts "u:rdh" optchar; do
+while getopts "u:rdsh" optchar; do
     case $optchar in
         u)  NMCLI_MOD_USER="$OPTARG"    ;;
         r)  NMCLI_RESET=1               ;;
         d)  NMCLI_REMOVE=1              ;;
+        s)  STRIP_DEPRECATED=1          ;;
         h)  usage; exit 0               ;;
         *)  usage; exit 1               ;;
     esac
@@ -82,7 +86,7 @@ shift $((OPTIND - 1))
 # Prepare to import either only the configurations supplied by user or
 # all configurations in current directory.
 OVPN_CONFS="ipvanish-*.ovpn"
-CONFS="${@:-$OVPN_CONFS}"
+CONFS="${*:-$OVPN_CONFS}"
 
 # If asked to delete VPN profiles, remove and exit before proceeding to add
 # new ones.
@@ -95,13 +99,18 @@ if [ -n "$NMCLI_RESET" ] || [ -n "$NMCLI_REMOVE" ]; then
 fi
 
 for conf in $CONFS; do
+    if [ -n "$STRIP_DEPRECATED" ]
+    then
+        sed -e "/${DEPRECATED_OPTS_RE}/d" "$conf" > "${conf}.tmp"
+        mv -- "${conf}.tmp" "$conf"
+    fi
     conf_id="${conf%.ovpn}"
     nmcli connection import type openvpn file "$conf"
     nmcli connection modify "$conf_id" connection.autoconnect FALSE
     if [ -n "$NMCLI_MOD_USER" ]; then
         nmcli connection modify "$conf_id" +vpn.data username="$NMCLI_MOD_USER"
     fi
-    new_cnt=$((new_cnt+1))
+    NEW_CNT=$((NEW_CNT+1))
 done
 
 print_status
